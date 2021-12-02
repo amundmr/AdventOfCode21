4b7f4039b7d45dc1b35d90d8ccc13138d10d7a59
amundmr <amund.raniseth@gmail.com>
2021-12-02 08:27:01 +0100

    solved day 2! So fun :D

---
+++ src/day2.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+pub fn day2() {
+    println!("Day 2 solution:");
+    let filename = "src/2input.txt";
+    println!("The input filename chosen is '{}'", filename);
+
+    let contents =
+        fs::read_to_string(filename).expect("Something went wrong when reading the file");
+    //println!("{}", contents);
+    //let splitlines: Vec<_> = contents.lines().collect();
+    //println!("{:?}", splitlines);
+
+    let mut depth: i32 = 0;
+    let mut horiz: i32 = 0;
+
+    for line in contents.lines() {
+        let split: Vec<_> = line.split(" ").collect();
+        let direction = split[0];
+        let distance = split[1].parse::<i32>().unwrap();
+        //println!("Direction: {}, amount: {}", direction, distance);
+        if direction == "down" {
+            depth += distance;
+        } else if direction == "up" {
+            depth -= distance;
+        } else if direction == "forward" {
+            horiz += distance;
+        }
+    }
+    println!("Final Depth: {}, Final Horizontal position: {}, Multiplied: {}", depth, horiz, depth*horiz);
+
+    // Part two
+    println!("Part two:");
+    let mut depth: i32 = 0;
+    let mut horiz: i32 = 0;
+    let mut aim: i32 = 0;
+
+    for line in contents.lines() {
+        let split: Vec<_> = line.split(" ").collect();
+        let direction = split[0];
+        let distance = split[1].parse::<i32>().unwrap();
+        //println!("Direction: {}, amount: {}", direction, distance);
+        if direction == "down" {
+            aim += distance;
+        } else if direction == "up" {
+            aim -= distance;
+        } else if direction == "forward" {
+            horiz += distance;
+            depth += aim*distance;
+        }
+    }
+    println!("Final Depth: {}, Final Horizontal position: {}, Multiplied: {}", depth, horiz, depth*horiz);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod day1;
+mod day2;
 
 fn main() {
-    day1::day1();
+    //day1::day1();
+    day2::day2();
 }
 
