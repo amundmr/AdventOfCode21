c7f32fdc81be000914a83fab6c265475065329db
amundmr <amund.raniseth@gmail.com>
2021-12-04 12:52:15 +0100

    done reading in tables/matrices

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,44 @@
-pub fn day2() {
-    println!("Day 4 solution:");
+use std::fs;
+
+pub fn day4() {
+    println!("----------------------");
+    println!("Day 4 part 1 solution:");
+    println!("----------------------");
+
+    let filename = "inputs/4.txt";
+    let contents = fs::read_to_string(filename).expect("Something went wrong while reading file");
+
+    let mut lines: Vec<_> = contents.lines().collect();
+    let len: usize = lines.len();
+
+    //println!("{:?}", lines);
+
+    let random_numbers: Vec<_> = lines[0].split(",").map(|x| x.parse::<usize>().unwrap()).collect();
+
+    lines.remove(0); //Delete the random numbers so it wont interfere with the table generation
+    lines.remove(0); //Delete the empty line
+
+
+    println!("The random numbers are: {:?}", random_numbers);
+
+    // Read in the matrices
+    let mut matrices: Vec<Vec<Vec<usize>>> = vec![];
+    //Format: [ [   [[],[],[]], 
+    //              [[],[],[]], 
+    //              [[],[],[]] ], ... ]
+    let mut curr_table: Vec<Vec<usize>> = vec![];
+    for line in lines {
+        if line.len() < 4 {
+            matrices.push(curr_table);
+            curr_table = vec![];
+            continue;
+        } else {
+            let row: Vec<usize> = line.split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect();
+            curr_table.push(row);
+        }
+    }
+    //println!("{:?}", matrices);
     
+    
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 //mod day1;
 //mod day2;
-mod day3;
-//mod day4;
+//mod day3;
+mod day4;
 //mod day5;
 //mod day6;
 
 fn main() {
     //day1::day1();
     //day2::day2();
-    day3::day3();
-    //day4::day4();
+    //day3::day3();
+    day4::day4();
 }
 
