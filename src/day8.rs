d1295eb360d8559b7c01678a8546eb437032f92f
amundmr <amund.raniseth@gmail.com>
2021-12-07 11:18:55 +0100

    ready for day 8

---
+++ src/day8.rs
@@ -0,0 +1,3 @@
+pub fn day8() {
+    println!("Day 8 solution");
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 //mod day4;
 //mod day5;
 //mod day6;
-mod day7;
+//mod day7;
+mod day8;
 
 fn main() {
     //day1::day1();
@@ -13,6 +14,7 @@ fn main() {
     //day4::day4();
     //day5::day5();
     //day6::day6();
-    day7::day7();
+    //day7::day7();
+    day8::day8();
 }
 
